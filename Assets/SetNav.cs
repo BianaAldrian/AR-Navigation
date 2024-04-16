bb2340dc1ae7ba0359267c3abe58c77ac7954b48
BianaAldrian <123650902+BianaAldrian@users.noreply.github.com>
2024-04-17 06:57:41 +0800

    first push

---
+++ Assets/SetNav.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNav : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject NavTarget;
+
+    private NavMeshPath path;
+    private LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMesh.CalculatePath(transform.position, NavTarget.transform.position, NavMesh.AllAreas, path);
+        line.positionCount = path.corners.Length;
+        line.SetPositions(path.corners);
+        line.enabled = true;
+    }
+}
